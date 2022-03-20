cc1c42756a99060aa4cede5a38490d6d6ce4a94c
Delile Sebastien <sebastiendelile@gmail.com>
2022-03-20 15:44:21 +0100

    fix formatPinyinSentence for undefined

--- src/utils/formatPinyinSentence.ts
+++ src/utils/formatPinyinSentence.ts
@@ -1,10 +1,12 @@
 import { formatPinyin } from './formatPinyin';
 
 export function formatPinyinSentence(pinyinSentence: string): string {
-  return pinyinSentence
-    .split(' ')
-    .map((pinyinAndTone) =>
-      formatPinyin(pinyinAndTone.slice(0, -1), Number(pinyinAndTone.at(-1)))
-    )
-    .join(' ');
+  return (
+    pinyinSentence
+      ?.split(' ')
+      .map((pinyinAndTone) =>
+        formatPinyin(pinyinAndTone.slice(0, -1), Number(pinyinAndTone.at(-1)))
+      )
+      .join(' ') ?? ''
+  );
 }

3318a64131a20f3a9c9816654c3cde8b44926aad
Delile Sebastien <sebastiendelile@gmail.com>
2022-07-10 13:45:47 +0200

    feat: add links to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { Children, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { BurgerMenuIcon } from '../icons/BurgerMenuIcon';
 import { CloseIcon } from '../icons/CloseIcon';
 
 export function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <header className="fixed top-0 w-full py-4 bg-primary text-primary-contrast shadow-md">
       <div className="first-child flex justify-between relative flex-wrap">
@@ -18,10 +20,19 @@ export function Header() {
       </div>
       <nav className="first-child flex justify-end">
         {isMenuOpen ? (
-          <ul className="w-max list-none flex flex-col items-center gap-3 mt-4 xs:flex-row">
-            {['Accueil', 'Test lecture', 'Test écriture'].map((item) => (
-              <li key={item}>{item}</li>
-            ))}
+          <ul
+            className="w-max list-none flex flex-col items-center gap-3 mt-4 xs:flex-row"
+            onClick={(): void => setIsMenuOpen((state) => !state)}
+          >
+            <li>
+              <Link to="/">Accueil</Link>
+            </li>
+            <li>
+              <Link to="/reading-test">Test de lecture</Link>
+            </li>
+            <li>
+              <Link to="/writing-test">Test d&apos;écriture</Link>
+            </li>
           </ul>
         ) : null}
       </nav>

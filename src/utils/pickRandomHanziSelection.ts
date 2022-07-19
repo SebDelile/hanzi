ef9879d30a6e35ee4c914bdc675d7a541349970b
Delile Sebastien <sebastiendelile@gmail.com>
2022-07-19 17:58:15 +0200

    refactor: optimize pickRandomHanziSelection

--- src/utils/pickRandomHanziSelection.ts
+++ src/utils/pickRandomHanziSelection.ts
@@ -6,12 +6,26 @@ export function pickRandomHanziSelection(
 ): HanziObject[] {
   const hanziSelectionIndexes: number[] = [];
   const hanziListLength = hanziList.length;
-  while (hanziSelectionIndexes.length < count) {
+
+  // negativeMode = pick the ones to exclude instead of the ones to include
+  // So the worse possible situation is when half of the item has already beeing picked
+  const isNegativeMode = count > hanziListLength / 2;
+  const hanziSelectionTargetedLength = isNegativeMode
+    ? hanziListLength - count
+    : count;
+
+  while (hanziSelectionIndexes.length < hanziSelectionTargetedLength) {
     const randomIndex = Math.floor(Math.random() * hanziListLength);
     if (!hanziSelectionIndexes.includes(randomIndex)) {
       hanziSelectionIndexes.push(randomIndex);
     }
   }
 
-  return hanziSelectionIndexes.map((index) => hanziList[index]);
+  const hanziSelection = isNegativeMode
+    ? hanziList.filter(
+        (_hanzi, index) => !hanziSelectionIndexes.includes(index)
+      )
+    : hanziSelectionIndexes.map((index) => hanziList[index]);
+
+  return hanziSelection;
 }
